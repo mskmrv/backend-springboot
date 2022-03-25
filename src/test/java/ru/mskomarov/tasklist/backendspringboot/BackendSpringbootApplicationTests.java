2b64ad0cabb10a6277c1844715121456446b7b40
Komarov Maksim <mx123@yandex.ru>
2022-03-25 19:02:20 +1000

    Initial commit

---
+++ src/test/java/ru/mskomarov/tasklist/backendspringboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mskomarov.tasklist.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
