7eb8b75fb47fa4ca411d7b06145d6ef63089cdd2
Komarov Maksim <mx123@yandex.ru>
2022-03-28 14:04:26 +1000

    created PriorityRepository

---
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/repo/PriorityRepository.java
@@ -0,0 +1,10 @@
+package ru.mskomarov.tasklist.backendspringboot.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.mskomarov.tasklist.backendspringboot.entity.Priority;
+
+@Repository
+public interface PriorityRepository extends JpaRepository<Priority, Long> {
+
+}
