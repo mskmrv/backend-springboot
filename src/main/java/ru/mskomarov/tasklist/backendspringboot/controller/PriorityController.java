7142490f5e47cd1d6a5e3b450454ded8ec768d14
Komarov Maksim <mx123@yandex.ru>
2022-03-29 12:11:18 +1000

    add validation into method add()

--- src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,5 +1,7 @@
 package ru.mskomarov.tasklist.backendspringboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.mskomarov.tasklist.backendspringboot.entity.Priority;
 import ru.mskomarov.tasklist.backendspringboot.repo.PriorityRepository;
@@ -21,7 +23,15 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority) {
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
