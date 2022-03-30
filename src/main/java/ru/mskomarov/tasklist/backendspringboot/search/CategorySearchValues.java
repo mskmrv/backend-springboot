1422174a6a5f8025a08db4fb22eaec94c5e695e4
Komarov Maksim <mx123@yandex.ru>
2022-03-30 10:33:55 +1000

    create search class

---
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/search/CategorySearchValues.java
@@ -0,0 +1,14 @@
+package ru.mskomarov.tasklist.backendspringboot.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategorySearchValues {
+    private String text;
+}
