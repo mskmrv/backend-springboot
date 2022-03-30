254cc697faf78a5dba6d97746cc7e50f304819a8
Komarov Maksim <mx123@yandex.ru>
2022-03-30 14:26:47 +1000

    add logger to PriorityController

--- src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,5 +1,7 @@
 package ru.mskomarov.tasklist.backendspringboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.NoSuchElementException;
 @RestController
 @RequestMapping("/priority")
 public class PriorityController {
+    private static final Logger logger = LoggerFactory.getLogger(PriorityController.class);
     private PriorityRepository priorityRepository;
 
     public PriorityController(PriorityRepository priorityRepository) {
@@ -24,11 +27,17 @@ public class PriorityController {
 
     @GetMapping("/all")
     public List<Priority> findAll() {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: findAll()");
+
         return priorityRepository.findAllByOrderByIdAsc();
     }
 
     @PostMapping("/add")
     public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: add()");
+
         if (priority.getId() != null && priority.getId() != 0) {
             return new ResponseEntity("id must be null", HttpStatus.NOT_ACCEPTABLE);
         }
@@ -46,6 +55,9 @@ public class PriorityController {
 
     @PutMapping("/update")
     public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: update()");
+
         if (priority.getId() == null || priority.getId() == 0) {
             return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
         }
@@ -63,6 +75,9 @@ public class PriorityController {
 
     @GetMapping("/id/{id}")
     public ResponseEntity<Priority> findById(@PathVariable Long id) {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: findById()");
+
         Priority priority = null;
 
         try {
@@ -77,6 +92,9 @@ public class PriorityController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: delete()");
+
         try {
             priorityRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
@@ -89,6 +107,9 @@ public class PriorityController {
 
     @PostMapping("/search")
     public ResponseEntity<List<Priority>> search(@RequestBody PrioritySearchValues prioritySearchValues) {
+        logger.info("\n-------------------------------");
+        logger.info("\nPriorityController: search()");
+
         return ResponseEntity.ok(priorityRepository.findByTitle(prioritySearchValues.getText()));
     }
 }
