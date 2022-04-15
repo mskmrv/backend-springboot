0e7ee5c2301b28e04ea4d67b7b975b06528a176b
Komarov Maksim <mx123@yandex.ru>
2022-04-15 17:00:14 +1000

    Create TaskService

---
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/service/TaskService.java
@@ -0,0 +1,44 @@
+package ru.mskomarov.tasklist.backendspringboot.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.mskomarov.tasklist.backendspringboot.entity.Task;
+import ru.mskomarov.tasklist.backendspringboot.repo.TaskRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class TaskService {
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Task add(Task task) {
+        return repository.save(task);
+    }
+
+    public Task update(Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Page findByParams(String title, Integer complited, Long priorityId, Long categoryId, PageRequest pageRequest) {
+        return repository.findByParams(title, complited, priorityId, categoryId, pageRequest);
+    }
+
+    public Task findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
