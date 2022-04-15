2026271d7464427fb4c4a89851620279854b56fa
Komarov Maksim <mx123@yandex.ru>
2022-04-15 17:36:29 +1000

    Add service-layer to StatController

--- src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/StatController.java
+++ src/main/java/ru/mskomarov/tasklist/backendspringboot/controller/StatController.java
@@ -4,26 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.mskomarov.tasklist.backendspringboot.entity.Stat;
-import ru.mskomarov.tasklist.backendspringboot.repo.StatRepository;
+import ru.mskomarov.tasklist.backendspringboot.service.StatService;
 
 @RestController
 public class StatController {
     private static final Logger logger = LoggerFactory.getLogger(StatController.class);
     private final Long defaultId = 1L;
-    private StatRepository statRepository;
+    private StatService statService;
 
-    public StatController(StatRepository statRepository) {
-        this.statRepository = statRepository;
+    public StatController(StatService statService) {
+        this.statService = statService;
     }
+
 
     @GetMapping("/stat")
     public ResponseEntity<Stat> findById() {
         logger.info("\n-------------------------------");
         logger.info("\nStatController: findById()");
 
-        return ResponseEntity.ok(statRepository.findById(defaultId).get());
+        return ResponseEntity.ok(statService.findById(defaultId));
     }
 }
